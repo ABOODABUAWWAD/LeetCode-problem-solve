398d21433a4ad92583c2448028cb868396256d69
ABOODABUAWWAD <aboodabuawwad309@gmail.com>
2024-06-21 11:16:54 +0300

    Time: 0 ms (100.00%), Space: 7.9 MB (99.47%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isPathCrossing(string path) {
+  /*  bool isPathCrossing(string path) {
         vector <int> y;
         y.push_back(0);
         vector <int> x;
@@ -39,8 +39,48 @@ for (int i = 0; i < path.length()+1; i++)
 		}
 	}
 	return false;
-    }
+    }*/
+bool isPathCrossing(string path) {
+	int y[10001];
+	y[0] = 0;
+	int x[10001];
+	x[0] = 0;
+	for (int i = 0; i < path.length(); i++)
+	{
+		if (path[i] == 'N')
+		{
+			y[i + 1] = y[i] + 1;
+			x[i + 1] = x[i];
+		}
+		else if (path[i] == 'S')
+		{
+			y[i + 1] = y[i] - 1;
+			x[i + 1] = x[i];
+		}
+		else if (path[i] == 'E')
+		{
+			x[i + 1] = x[i] + 1;
+			y[i + 1] = y[i];
+		}
+		else if (path[i] == 'W')
+		{
+			x[i + 1] = x[i] - 1;
+			y[i + 1] = y[i];
+		}
+	}
+	for (int i = 0; i < path.length()+1; i++)
+	{
+		for (int j = 0; j < path.length()+1; j++)
+		{
+			if (x[i] == x[j] && y[i] == y[j] && i != j)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
 
+}
     
     
 };
