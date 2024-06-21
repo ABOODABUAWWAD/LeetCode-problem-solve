9b29b5a32be2bf5b075fb5750b4d84cf8b1a7eb6
ABOODABUAWWAD <aboodabuawwad309@gmail.com>
2024-06-21 10:25:57 +0300

    Time: 0 ms (100.00%), Space: 7.6 MB (86.67%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length_s1=0,length_s2;
+        for(int i=0;i<s.length();i++){
+            if(s[i]!=32)
+            {
+                length_s1++;
+            }
+            else if(s[i]==32)
+            {
+                if(length_s1!=0) 
+                {
+                length_s2=length_s1;
+                }
+            length_s1=0;
+            }
+        }
+          if(length_s1!=0)
+          return length_s1;
+          else
+          return length_s2;
+    }
+};
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
