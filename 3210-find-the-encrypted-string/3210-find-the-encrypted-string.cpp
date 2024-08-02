1fc2e0932e42c03f3df00d24155adc932cb5bec3
ABOODABUAWWAD <aboodabuawwad309@gmail.com>
2024-08-02 12:05:49 +0300

    Time: 5 ms (30.72%), Space: 8.2 MB (21.31%) - LeetHub

---
+++ 3210-find-the-encrypted-string/3210-find-the-encrypted-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string getEncryptedString(string s, int k) {
+        string EncryptedStr = s;
+        for (int i = 0; i < s.length(); i++) {
+            EncryptedStr[i] = s[(i + k) % s.length()];
+        }
+        return EncryptedStr;
+    }
+};
