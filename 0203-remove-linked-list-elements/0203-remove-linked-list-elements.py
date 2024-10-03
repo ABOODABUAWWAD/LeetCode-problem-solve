8ab18410a02efea38a5fdfe73bcff80629adf468
ABOODABUAWWAD <aboodabuawwad309@gmail.com>
2024-10-03 15:33:10 +0300

    Time: 52 ms (12.87%), Space: 18.3 MB (86.33%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -7,14 +7,10 @@ class Solution(object):
         while cur:
             if cur.val==val:
                 prevcur.next=cur.next
-                temp=cur
                 cur=prevcur.next
-                del temp
             else:
                 prevcur=cur
                 cur=cur.next
         if head.val ==val:
-            temp=head
             head=head.next
-            del temp
         return head
