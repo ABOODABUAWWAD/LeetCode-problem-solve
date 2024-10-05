53ae20841ba19dd88d63b7e5add848297440065b
ABOODABUAWWAD <aboodabuawwad309@gmail.com>
2024-10-05 03:08:08 +0300

    Time: 59 ms (84.42%), Space: 11.6 MB (90.08%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,21 +1,18 @@
 class Solution(object):
     def isValidSudoku(self, board):
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        squares = [set() for _ in range(9)]  
+        rows=[set() for i in range(9)]
+        cols=[set() for i in range(9)]
+        squares=[set() for i in range(9)]
+
         for i in range(9):
             for j in range(9):
-                value = board[i][j]
-                if value == '.':
+                val=board[i][j]
+                if val=='.':
                     continue
-                if value in rows[i]:
+                SquareIndex=i//3*3+j//3
+                if val in rows[i] or val in cols[j] or val in squares[SquareIndex]:
                     return False
-                rows[i].add(value)
-                if value in cols[j]:
-                    return False
-                cols[j].add(value)
-                square_index = (i // 3) * 3 + (j // 3)
-                if value in squares[square_index]:
-                    return False
-                squares[square_index].add(value)
+                rows[i].add(val)
+                cols[j].add(val)
+                squares[SquareIndex].add(val)
         return True
