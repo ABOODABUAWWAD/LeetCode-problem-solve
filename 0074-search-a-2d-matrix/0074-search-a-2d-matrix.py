ec941f8b21b823f3c7af921fb82254b9a22cfeab
ABOODABUAWWAD <aboodabuawwad309@gmail.com>
2024-10-16 17:23:39 +0300

    Time: 27 ms (54.77%), Space: 12.1 MB (37.61%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -4,20 +4,15 @@ class Solution(object):
         min_row = 0
         max_row = len(lst)-1
         TRI = 0
-
         while min_row <= max_row:
             mid_row = (max_row+min_row)//2
             if lst[mid_row][0] <= target and lst[mid_row][l] >= target:
                 TRI = mid_row
                 break
-
             elif lst[mid_row][l] < target:
                 min_row = mid_row + 1
-
             elif lst[mid_row][0] > target:
                 max_row = mid_row - 1
-
-        
         left = 0
         right = l
         while left <= right:
