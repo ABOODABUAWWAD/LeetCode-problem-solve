2ffa5ca7076b1d6bf47009b98834c01eb535e93a
ABOODABUAWWAD <aboodabuawwad309@gmail.com>
2024-10-17 01:42:28 +0300

    Time: 496 ms (96.66%), Space: 73.4 MB (8.74%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+class TimeMap(object):
+
+    def __init__(self):
+        self.dic=defaultdict(list)
+
+    def set(self, key, value, timestamp):
+        self.dic[key].append([timestamp, value])
+
+    def get(self, key, timestamp):
+        if key not in self.dic:
+            return ""
+        values = self.dic[key]
+        if values[-1][0]<=timestamp:
+            return values[-1][-1]
+        left, right = 0, len(values) - 1
+        res = ""
+        while left <= right:
+            mid = (left + right) // 2
+            if values[mid][0] <= timestamp:
+                res = values[mid][1]
+                left = mid + 1
+            else:
+                right = mid - 1
+        return res
