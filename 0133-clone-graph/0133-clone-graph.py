2075275421d14e54c347303ae01f03e68293bcd9
ABOODABUAWWAD <aboodabuawwad309@gmail.com>
2024-10-20 00:00:07 +0300

    Time: 28 ms (83.31%), Space: 11.8 MB (89.81%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -2,13 +2,13 @@ class Solution(object):
     def cloneGraph(self, node):
         if not node:
             return None
-        copiesnode={}
-        def clone(node):
-            if node in copiesnode:
-                return copiesnode[node]
-            copy=Node(node.val)
-            copiesnode[node]=copy
-            for n in node.neighbors:
-                copy.neighbors.append(clone(n))
-            return copy
-        return clone(node)
+        q = deque([node])
+        clone_map = {node:Node(node.val)}
+        while q:
+            curr_node = q.popleft()  
+            for neighbor in curr_node.neighbors:
+                if neighbor not in clone_map:
+                    clone_map[neighbor] = Node(neighbor.val)
+                    q.append(neighbor)
+                clone_map[curr_node].neighbors.append(clone_map[neighbor])
+        return clone_map[node]
